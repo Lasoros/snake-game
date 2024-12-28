7df1860c80d4df3eb25a0312b210dffb205cce27
Thomas Wimer <ipodtouch_guy@hotmail.com>
2024-12-28 17:03:23 -0500

    Game Logic

--- src/components/GameState.js
+++ src/components/GameState.js
@@ -4,6 +4,29 @@ const GameState = () => {
  
     const [score, setScore] = useState(0)
     const [highScore, setHighScore] = useState(parseInt(localStorage.getItem('highScore')) || 0)
+    const [gameOver, setGameOver] = useState(false)
+    const [collision, setCollisionType] = useState("")
+
+    return (
+        <div>
+            <p>Score: {score}</p>
+            <p>High Score: {highScore}</p>
+
+            {
+                gameOver && (
+                    <div>
+                        <p>Game Over! {collision == "wall" ? "Wall Struck, You Lose!" : "Snake No Taste Good"}</p>
+                        <p>Please Press Enter To Reset The Game</p>
+                    </div>
+                )
+            }{
+                !gameOver && (
+                    <GameState/>
+                )
+            }
+        </div>
+    )
+
 }
 
 export default GameState
