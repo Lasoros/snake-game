05fc56d1cc24be9553ebb44541c478e5cdc40900
Thomas Wimer <ipodtouch_guy@hotmail.com>
2024-12-28 17:30:47 -0500

    Added Snake & Treat

--- src/components/GamePieces.jsx
+++ src/components/GamePieces.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const GamePieces = () => {
   const canvasRef = useRef();
@@ -10,6 +10,42 @@ const GamePieces = () => {
   ]);
 
   const [direction, setDirection] = useState(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    // console.log(canvasRef.current); allows grabbing of properties of canvas when editing
+
+    const cntxt = canvas.getContext("2d");
+
+    const drawSnake = () => {
+      snake.forEach((snakePart) => {
+        cntxt.beginPath();
+        cntxt.rect(snakePart.x, snakePart.y, 14, 14);
+        cntxt.fillSyle = "#90EE90";
+        cntxt.fill();
+        cntxt.closePath();
+      });
+    };
+
+    const drawApple = () => {
+      cntxt.beginPath();
+      cntxt.rect(apple.x, apple.y, 14, 14);
+      cntxt.fillSyle = "#FF0000";
+      cntxt.fill();
+      cntxt.closePath();
+    };
+
+    const interval = setInterval(() => {
+      cntxt.clearRect(0, 0, canvas.width, canvas.height);
+      drawSnake();
+      drawApple();
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [snake, direction]);
 
   return (
     <div>
